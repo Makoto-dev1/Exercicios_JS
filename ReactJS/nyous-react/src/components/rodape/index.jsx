250722c2824034bf60da8f0c8c195e1d7b821708
Makoto-dev1 <kfzm4k0y@gmail.com>
2020-10-26 17:52:02 -0300

    Aula 10, paginas e definição de rotas

--- ReactJS/nyous-react/src/components/rodape/index.jsx
+++ ReactJS/nyous-react/src/components/rodape/index.jsx
@@ -1,10 +1,11 @@
+  
 import React from 'react';
 
 const Rodape = () => {
-    return(
-        <footer className="text-center" style={{ marginTop : '70px'}} >
-            <h1>Senai informática 132</h1>
-            <small>Desenvolvido por <a href="https:/github.com/makoto-dev1" className="hreef">Marcelo Makoto</a></small>
+    return (
+        <footer className="text-center" style={{ marginTop : '70px'}}>
+                <h1>Senai informática 132</h1>
+                <small>Desenvolvido por <a href="https:/github.com/makoto-dev1">Marcelo Makoto</a></small>
         </footer>
     )
 }
--- ReactJS/nyous-react/src/index.js
+++ ReactJS/nyous-react/src/index.js
@@ -2,12 +2,27 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import Home from './pages/home';
+import Login from './pages/login';
+import Cadastrar from './pages/cadastrar';
+import Naoencontrada from './pages/naoencontrada';
 import reportWebVitals from './reportWebVitals';
 
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+
+const routing = (
+  <Router>
+      <Switch>
+        <Route exact path='/' component={Home} />
+        <Route path='/login' component={Login} />
+        <Route path='/cadastrar' component={Cadastrar} />
+        <Route component={Naoencontrada} />
+      </Switch>
+  </Router>
+) 
+
 ReactDOM.render(
-  <React.StrictMode>
-    <Home />
-  </React.StrictMode>,
+  routing,
   document.getElementById('root')
 );
 
@@ -15,3 +30,4 @@ ReactDOM.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
