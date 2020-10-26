16df6614709f29179859a2785a59966719b83810
Makoto-dev1 <kfzm4k0y@gmail.com>
2020-10-26 10:42:51 -0300

    Aula 7 e 8 finalizadas

---
+++ ReactJS/nyous-react/src/components/rodape/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Rodape = () => {
+    return(
+        <footer className="text-center" style={{ marginTop : '70px'}} >
+            <h1>Senai informática 132</h1>
+            <small>Desenvolvido por <a href="https:/github.com/makoto-dev1" className="hreef">Marcelo Makoto</a></small>
+        </footer>
+    )
+}
+
+export default Rodape;
---
+++ ReactJS/nyous-react/src/pages/home/index.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import Menu from '../../components/menu';
+import Rodape from '../../components/rodape';
+
+const Home = () => {
+    return(
+        <div>
+            <Menu />
+            <h1>Home</h1>
+            <Rodape />
+        </div>
+    )
+}
+
+export default Home;
